1f11511d214010304e6b0685a3a564c728e0e2ec
Soumyojyoti Majumdar <gtechxd@gmail.com>
2022-08-29 16:02:22 +0530

    updated card component and test script of card and ReadME

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -2,12 +2,27 @@ import { shallow } from "enzyme";
 import Card from './index';
 
 it("renders without crashing without header", () => {
-    shallow( <Card hideHeader>ok</Card>);
+    const props = {
+      hideHeader:true,
+    }
+    shallow( <Card {...props}>ok</Card>);
   });
   
-  it("renders with header", () => {
-    const wrapper = shallow( <Card title="Henlo">ok</Card>);
-    const welcome = "Henlo";
-    expect(wrapper.contains(welcome)).toEqual(true);
+  it("renders with header title", () => {
+    const props = {
+      title:"Hi Card",
+    }
+    const wrapper = shallow( <Card {...props}>ok</Card>);
+    const title = "Hi Card";
+    expect(wrapper.contains(title)).toEqual(true);
+  });
+  
+  it("renders with loading", () => {
+    const props = {
+      title:"Hi Card",
+      loading: true
+    }
+    const wrapper = shallow( <Card {...props}>ok</Card>);
+    expect(wrapper.hasClass('isLoading')).toEqual(true);
   });
   
