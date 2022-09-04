213189f9deceb9ba5d95e94a50fb4af08fd86530
Soumyojyoti Majumdar <gtechxd@gmail.com>
2022-09-05 00:54:24 +0530

    updated with animation

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { SwitchTransition, CSSTransition } from 'react-transition-group'; // ES6
 import "./style.scss";
 
 function Tabs({ active, options, className, handleTabChange }) {
     return (
         <div className={`rnd-tabs ${className}`}>
             <div className='rnd-tab-options'>
-                {options.map((item, indx) => <div className={`rnd-tab-name ${active === indx ? 'rnd-tab-name-active' : ''}`} key={JSON.stringify(item)} onClick={() => handleTabChange(indx)} >{item.tabName}</div>)}
+                {options.map((item, indx) => <div className={`rnd-tab-name ${active === indx ? 'rnd-tab-name-active' : ''}`} key={JSON.stringify(item)} onClick={() => handleTabChange(indx)} >{item.tabName}{active === indx && <span></span>}</div>)}
             </div>
-            <div className='rnd-tab-body'>
-                {options[active].content}
-            </div>
+
+            <SwitchTransition>
+                <CSSTransition
+                    key={options[active].tabName.toLowerCase()}
+                    addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
+                    classNames='rnd-fade'
+                >
+                    <div className='rnd-tab-body'>
+                        {options[active].content}
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
+
+
         </div>
     )
 }
